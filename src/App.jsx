1fe3fb6bdb4c2a13c29c78aec3b5e1983e1a0b6e
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2024-09-12 14:20:08 +0200

    basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Content from "./components/Content";
+import styles from "./App.module.scss";
 
 function App() {
   return (
-    <div>
-      <h1>Cookchef</h1>
+    <div className={`d-flex flex-column ${styles.appContainer}`}>
+      <Header />
+      <Content />
+      <Footer />
     </div>
   )
 }
