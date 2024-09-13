4748f44f6aeadd67d46adcc7872e50155572c9b8
Gaëtan Massart <129839243+gtn-massart@users.noreply.github.com>
2024-09-13 09:28:19 +0200

    header + footer layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Content from "./components/Content";
+// import Content from "./components/Content";
 import styles from "./App.module.scss";
 
 function App() {
   return (
     <div className={`d-flex flex-column ${styles.appContainer}`}>
       <Header />
-      <Content />
+      {/* <Content /> */}
       <Footer />
     </div>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,13 @@
+import styles from "./Footer.module.scss"
+
 function Footer() {
-  return <div className="b2">Footer</div>
+  return (
+    <footer>
+      <p className={`${ styles.footer } d-flex flex-row align-items-center justify-content-center p-20`}>
+        Copyright © 2024 Cookchef
+      </p>
+    </footer>
+    ) 
 }
 
 export default Footer;
